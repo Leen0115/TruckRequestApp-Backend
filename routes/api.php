e6220756de28fe571f885f191bb3891ff3fc16a2
Leen Alqulayti <leenalqulayti@gmail.com>
2025-05-13 16:23:18 +0300

    updated version

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,14 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TruckRequestController;
 use App\Http\Controllers\EmailController;
 use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\UserController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return response()->json($request->user());
+});
+Route::middleware('auth:sanctum')->put('/user/update', [UserController::class, 'update']);
 
 Route::get('/truck-requests', [TruckRequestController::class, 'allRequests']);
 Route::post('/send-email', [EmailController::class, 'send']); // ارسال ايميل للمستخدم
@@ -18,6 +23,9 @@ Route::get('/notifications', [NotificationController::class, 'index']);
 Route::delete('/notifications/{id}', function ($id) {
     \Illuminate\Notifications\DatabaseNotification::find($id)?->delete();
     return response()->json(['message' => 'Notification deleted']);
+Route::get('/user', function (Request $request) {
+    return $request->user();
+});
 });
 
 Route::middleware('auth:sanctum')->group(function () {
